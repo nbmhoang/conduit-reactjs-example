edbbc0db2d952d84c11af76a98272e44ed9e1683
expressyoona <nbmhoang.17it1@sict.udn.vn>
2020-07-09 11:50:05 +0700

    Loading comment's article

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,27 +3,34 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Avatar, Col, Row, Tag } from 'antd';
 import { Link } from 'react-router-dom';
 
+import PageHeaderLoggedIn from '../PageHeaderLoggedIn';
 import PageHeader from '../PageHeader';
+import CommentBox from './Comment';
+import UserComment from './CommentList';
 import {
-    LOAD_ARTICLE_PAGE
+    LOAD_ARTICLE_PAGE,
+    LOAD_ARTICLE_COMMENT
 } from '../../constants/action';
 
 
-export default function Article({ match, location }) {
-    // console.log(match);
+
+export default function Article({ match: {params: { id}}}) {
+
+    const { title, avatar, createdAt, body, user, tagList, comments, slug } = useSelector(state => state.article);
     
-    const { title, avatar, createdAt, body, user, tagList } = useSelector(state => state.article);
+    const { isLoggedIn } = useSelector(state => state.user);
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch({ type: LOAD_ARTICLE_PAGE, slug: match.params.id });
+        dispatch({ type: LOAD_ARTICLE_PAGE, slug: id });
+        dispatch({ type: LOAD_ARTICLE_COMMENT, slug: id});
     }, []);
 
     return (
         <Row className="article-detail">
             <Col span={24}>
-                <Col span={12} offset={6}>
-                    <PageHeader />
+                <Col>
+                    {isLoggedIn ? <PageHeaderLoggedIn /> : <PageHeader />}
                 </Col>
                 <div className="article-banner">
                     <div className="banner-content">
@@ -48,9 +55,15 @@ export default function Article({ match, location }) {
                     })}
                 </div>
                 <hr />
-                <Col span={12} offset={4} style={{marginTop: "40px"}}>
-                    <Link to='/signin'>Sign in</Link> or <Link to='/signup'>sign up</Link> to add comments on this article.
-                </Col>
+                {isLoggedIn ? 
+                    <Col span={20} offset={2}>
+                        <CommentBox />
+                        <UserComment slug={slug} commentList={comments} />
+                    </Col> : 
+                    <Col span={12} offset={4} style={{marginTop: "40px"}}>
+                        <Link to='/signin'>Sign in</Link> or <Link to='/signup'>sign up</Link> to add comments on this article.
+                    </Col>
+                }
             </Col>
         </Row>
     )
