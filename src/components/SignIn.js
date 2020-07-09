22991ab7fc0e475244e66bd09dc04ef39c1c65b9
expressyoona <nbmhoang.17it1@sict.udn.vn>
2020-07-09 11:53:06 +0700

    Handle login & Add new page header after logged in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,65 +2,87 @@ import React, { useEffect } from 'react';
 import { Form, Input, Button, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-dom';
+import { Redirect } from 'react-router-dom';
 
 import PageHeader from './PageHeader';
 import {
-    LOGIN
+    LOGIN,
+    LOAD_CURRENT_USER
 } from '../constants/action';
 
 
 function SignIn() {
 
-    const { token } = useSelector(state => state.user);
+    const { isLoggedIn, errors } = useSelector(state => state.user);
     const dispatch = useDispatch();
-    /*
-    if (token) {
-        return <Redirect to='/' />
-    }
-    */
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            dispatch({ type: LOAD_CURRENT_USER })
+        }
+        
+    }, []);
+    
 
     const submitForm = values => {
-        // console.log(values);
         const data = {
             user: {
                 email: values.email,
                 password: values.password
             }
         }
-        console.log(data);
         dispatch({ type: LOGIN, payload: data });
     }
 
+    if(isLoggedIn) {
+         return <Redirect to="/" />
+    }
+
+    
+    const Error = () => {       
+        if (!errors) {
+            return null;
+        }
+        return (
+            <ul>
+            {Object.keys(errors).map(key => {
+                return <li className="error">{key} {errors[key].join()}</li>
+            })}
+            </ul>
+        )
+    }
+    
+
     return (
         <>
-            <PageHeader key={['signin']} />
-            <Row>
-                <Col span={6} offset={9}>
-                    <center>
-                        <h1>Sign In</h1>
-                        <Link className="hint-text" to="/signup">Need an account?</Link>
-                    </center>
-                    <Form
-                    onFinish={submitForm}>
-                        <Form.Item
-                            name="email"
-                            rules={[{ required: true, message: 'Email can\'t be blank' }]}>
-                            <Input placeholder="Email" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: 'Password can\'t be blank' }]}>
-                            <Input.Password visibilityToggle={false} placeholder="Password" />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button className="button-submit" type="primary" htmlType="submit" style={{float: "right"}}>
-                                Sign in
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Col>
-            </Row>
+     
+                <PageHeader key={['signin']} />
+                <Row>
+                    <Col span={6} offset={9}>
+                        <center>
+                            <h1>Sign In</h1>
+                            <Link className="hint-text" to="/signup">Need an account?</Link>
+                        </center>
+                        <Error />
+                        <Form
+                        onFinish={submitForm}>
+                            <Form.Item
+                                name="email">
+                                <Input placeholder="Email" />
+                            </Form.Item>
+                            <Form.Item
+                                name="password" >
+                                <Input.Password visibilityToggle={false} placeholder="Password" />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button className="button-submit" type="primary" htmlType="submit" style={{float: "right"}}>
+                                    Sign in
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Row>
+            
         </>
     )
 }
