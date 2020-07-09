7a6a1097da3bccbd839e24301c4e8600b5acfcb7
expressyoona <nbmhoang.17it1@sict.udn.vn>
2020-07-09 11:52:30 +0700

    Add new actions

--- src/constants/action.js
+++ src/constants/action.js
@@ -1,6 +1,7 @@
 // User action
 export const LOGIN = 'LOGIN';
-export const LOGGIN_SUCCESS = 'LOGGIN_SUCCESS';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
 
 export const LOGOUT = 'LOGOUT';
 export const REGISTER = 'REGISTER';
@@ -24,7 +25,13 @@ export const CLICK_ON_TAB = 'CLICK_ON_TAB';
 export const MODIFY_TAB = 'MODIFY_TAB';
 export const REFRESH_TABS = 'REFRESH_TABS';
 
+export const LOAD_CURRENT_USER = 'LOAD_CURRENT_USER';
+export const RENDER_CURRENT_USER = 'RENDER_CURRENT_USER';
 
+export const LOAD_ARTICLE_COMMENT = 'LOAD_ARTICLE_COMMENT';
+export const RENDER_ARTICLE_COMMENT = 'RENDER_ARTICLE_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+/*
 export function loadArticle() {
     return {
         type: LOAD_ARTICLE
@@ -36,3 +43,4 @@ export function loadPopularTag() {
         type: LOAD_POPULAR_TAG
     };
 }
+*/
